6bc5b6f54e9fee3333b648fa943086f4de579828
arkiver <arkiver@protonmail.com>
2023-08-07 17:04:45 +0200

    Version 20230807.15. Connect timeout 0.3.

--- pipeline.py
+++ pipeline.py
@@ -59,7 +59,7 @@ if not WGET_AT:
 #
 # Update this each time you make a non-cosmetic change.
 # It will be added to the WARC files and reported to the tracker.
-VERSION = '20230807.14'
+VERSION = '20230807.15'
 USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/116.0'
 TRACKER_ID = 'skyblog'
 TRACKER_HOST = 'legacy-api.arpa.li'
@@ -255,7 +255,7 @@ class WgetArgs(object):
             '--no-parent',
             '--page-requisites',
             '--timeout', '30',
-            '--connect-timeout', '0.2',
+            '--connect-timeout', '0.3',
             '--tries', 'inf',
             '--domains', 'skyblog.com,skyrock.net,sk.mu',
             '--span-hosts',
